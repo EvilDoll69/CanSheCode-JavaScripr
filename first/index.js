be8eadb8b46e1e8e7757d5a9b3c6d6b0a5724414
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-04 19:16:20 -0500

    training

--- first/index.js
+++ first/index.js
@@ -1,6 +1,17 @@
-let user = prompt("What is your name")
+// let user = prompt("What is your name")
 
-alert("Hello! " + user);
+// alert("Hello! " + user);
 
-let color = prompt("What is your favourite color");
-alert(`${user} is amazing and ${color} is my favourite color`);
+// let color = prompt("What is your favourite color");
+// alert(`${user} is amazing and ${color} is my favourite color`);
+
+const hello = function() {
+  alert("Hello! ");
+};
+
+const user  = function() {
+  let userName = prompt("What is your name?")
+}
+
+user();
+hello();
