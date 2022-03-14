69f2b55a56016f672f4225a0379443613deda61e
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-14 14:12:24 -0400

    Practice JS

--- first/index.js
+++ first/index.js
@@ -5,10 +5,18 @@
 // let color = prompt("What is your favourite color");
 // alert(`${user} is amazing and ${color} is my favourite color`);
 
-const hello = function() {
-  alert("Hello! ");
-};
+// const hello = function() {
+//   alert("Hello! ");
+// };
 
-const user  = function() {
-  let userName = prompt("What is your name?")
+// const user  = function() {
+//   let userName = prompt("What is your name?")
+// }
+
+const age = function() {
+  let number = prompt("How old are you?");
+  alert(`User is ${number} years old`)
 }
+
+age();
+
