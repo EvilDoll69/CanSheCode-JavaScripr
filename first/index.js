9b1b6b8c9c6b71a4e59574e7382b84ce0144549c
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-14 14:37:49 -0400

    Keep training JS

--- first/index.js
+++ first/index.js
@@ -13,10 +13,40 @@
 //   let userName = prompt("What is your name?")
 // }
 
-const age = function() {
-  let number = prompt("How old are you?");
-  alert(`User is ${number} years old`)
+// const age = function() {
+//   let number = prompt("How old are you?");
+//   alert(`User is ${number} years old`)
+// }
+
+// age();
+
+
+// const userName = function() {
+//   let user = prompt("What is your name?")
+//   hello(user);
+// }
+
+// const hello = function(user) {
+//   alert(`Hello ${user}`)
+// }
+
+// userName();
+
+// const age = function() {
+//   let number = prompt("How old are you?");
+//   message(number);
+
+// }
+
+// const message = function(number) {
+//   alert(`You are ${number} years old`)
+// };
+
+// age();
+
+const calcAge = function() {
+  let age = prompt("How old are you?");
+  alert(`Your age is ${age * 12} in months`)
 }
 
-age();
-
+calcAge();
