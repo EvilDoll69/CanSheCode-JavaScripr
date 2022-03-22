5b0465f9f8d5963ea3e57ecde167bc7e808eda53
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-22 16:48:10 -0400

    User can press the button and add new todo to the list

--- to-do_list_Project/index.js
+++ to-do_list_Project/index.js
@@ -1,1 +1,15 @@
- 
+const btn = document.querySelector(".btn");
+const inputField = document.querySelector(".input-field");
+const listItem = document.querySelector(".list-container");
+
+const addAction = btn.addEventListener("click", function() {
+  const newItemInList = document.createElement("li");
+  newItemInList.innerText = inputField.value;
+  newItemInList.classList.add("list-item");
+  listItem.appendChild(newItemInList);
+  inputField.value = "";
+
+  newItemInList.addEventListener("dblclick", function() {
+    newItemInList.classList.add("crossed-text");
+  })
+})
