1a50d8011304292dd1c6a0241da079a0d3486e90
Tatyana Fediushyn <furer@Tatyanas-MacBook-Pro.local>
2022-03-22 17:27:26 -0400

    Adaptation to devices

--- to-do_list_Project/index.js
+++ to-do_list_Project/index.js
@@ -2,16 +2,19 @@ const btn = document.querySelector(".btn");
 const inputField = document.querySelector(".input-field");
 const listItem = document.querySelector(".list-container");
 
-const addAction = btn.addEventListener("click", function() {
+const addAction = btn.addEventListener("click", () => {
   const newItemInList = document.createElement("li");
   newItemInList.innerText = inputField.value;
   newItemInList.classList.add("list-item");
   listItem.appendChild(newItemInList);
   inputField.value = "";
 
-  newItemInList.addEventListener("click", function() {
+  newItemInList.addEventListener("click", () => {
     newItemInList.classList.add("crossed-item");
   })
 
-  
+  newItemInList.addEventListener("dblclick", () => {
+    // newItemInList.remove();
+    listItem.removeChild(newItemInList);
+  })
 })
